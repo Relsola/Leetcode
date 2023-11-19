371d4a4a6e04dfd283cdd541f31ad4f0c1bb8de8
Relsola <sola001018@gmail.com>
2023-11-19 23:09:00 +0800

    TypeScript

--- TypeScript/0977-有序数组的平方.ts
+++ TypeScript/0977-有序数组的平方.ts
@@ -2,7 +2,7 @@ function sortedSquares(nums: number[]): number[] {
     let left: number = 0,
         size: number = nums.length,
         right: number = size - 1;
-    const arr = new Array(size);
+    const arr: number[] = new Array(size);
     while (size > 0) {
         if (Math.abs(nums[left]) > nums[right]) {
             arr[--size] = nums[left++] ** 2;
