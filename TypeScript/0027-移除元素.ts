e282887d4ec340b63f5baeabac98b677ce0c8082
Relsola <sola001018@gmail.com>
2023-11-20 23:27:29 +0800

    TypeScript

--- TypeScript/0027-移除元素.ts
+++ TypeScript/0027-移除元素.ts
@@ -1,3 +1,4 @@
+// while 循环
 function removeElement(nums: number[], val: number): number {
     let fast: number = 0;
     let slow: number = 0;
@@ -10,4 +11,16 @@ function removeElement(nums: number[], val: number): number {
         nums[slow++] = nums[fast++];
     }
     return slow;
+}
+
+// for 循环
+function removeElementO(nums: number[], val: number): number {
+    let k: number = 0;
+    for (let i = 0, n = nums.length; i < n; ++i) {
+        if (nums[i] === val) {
+            continue;
+        }
+        nums[k++] = nums[i];
+    }
+    return k;
 }
