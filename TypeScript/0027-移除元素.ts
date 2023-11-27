3f119bcadad4fc2184f2a5388dcde4b082fa8569
Relsola <sola001018@gmail.com>
2023-11-27 23:56:41 +0800

    TypeScript

--- TypeScript/0027-移除元素.ts
+++ TypeScript/0027-移除元素.ts
@@ -1,26 +1,26 @@
 // while 循环
 function removeElement(nums: number[], val: number): number {
-    let fast: number = 0;
-    let slow: number = 0;
-    const n: number = nums.length;
-    while (fast < n) {
-        if (nums[fast] === val) {
-            fast++;
-            continue;
-        }
-        nums[slow++] = nums[fast++];
-    }
-    return slow;
+	let fast: number = 0;
+	let slow: number = 0;
+	const n: number = nums.length;
+	while (fast < n) {
+		if (nums[fast] === val) {
+			fast++;
+			continue;
+		}
+		nums[slow++] = nums[fast++];
+	}
+	return slow;
 }
 
 // for 循环
 function removeElementO(nums: number[], val: number): number {
-    let k: number = 0;
-    for (let i = 0, n = nums.length; i < n; ++i) {
-        if (nums[i] === val) {
-            continue;
-        }
-        nums[k++] = nums[i];
-    }
-    return k;
+	let k: number = 0;
+	for (let i = 0, n = nums.length; i < n; ++i) {
+		if (nums[i] === val) {
+			continue;
+		}
+		nums[k++] = nums[i];
+	}
+	return k;
 }
--- TypeScript/0704-二分查找.ts
+++ TypeScript/0704-二分查找.ts
@@ -1,16 +1,16 @@
 function search(nums: number[], target: number): number {
-    let left: number = 0,
-        right: number = nums.length - 1;
-    while (left <= right) {
-        const mid = left + ((right - left) >> 1);
-        const num: number = nums[mid];
-        if (num < target) {
-            left = mid + 1;
-        } else if (num > target) {
-            right = mid - 1;
-        } else {
-            return mid;
-        }
-    }
-    return -1;
+	let left: number = 0,
+		right: number = nums.length - 1;
+	while (left <= right) {
+		const mid = left + ((right - left) >> 1);
+		const num: number = nums[mid];
+		if (num < target) {
+			left = mid + 1;
+		} else if (num > target) {
+			right = mid - 1;
+		} else {
+			return mid;
+		}
+	}
+	return -1;
 }
