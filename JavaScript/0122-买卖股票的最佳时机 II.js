4be524412f6a3635c6194f9630b9187721fd3a2e
Relsola <sola001018@gmail.com>
2023-12-11 22:15:41 +0800

    JavaScript

--- JavaScript/0122-买卖股票的最佳时机 II.js
+++ JavaScript/0122-买卖股票的最佳时机 II.js
@@ -1,4 +1,5 @@
 /**
+ * @description 贪心算法
  * @param {number[]} prices
  * @return {number}
  */
@@ -12,4 +13,23 @@ function maxProfit(prices) {
 		}
 	}
 	return result;
+}
+
+/**
+ * @description 动态规划
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+	// 滚动数组
+	// have: 第i天持有股票最大收益; notHave: 第i天不持有股票最大收益
+	let n = prices.length,
+		have = -prices[0],
+		notHave = 0;
+	for (let i = 1; i < n; i++) {
+		have = Math.max(have, notHave - prices[i]);
+		notHave = Math.max(notHave, have + prices[i]);
+	}
+	// 最终手里不持有股票才能保证收益最大化
+	return notHave;
 }
