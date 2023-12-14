7c68ce1b55c0bf2fcc6771bb8a1ad9873579be8c
Relsola <sola001018@gmail.com>
2023-12-14 22:38:37 +0800

    JavaScript

--- JavaScript/0053-最大子数组和.js
+++ JavaScript/0053-最大子数组和.js
@@ -1,4 +1,5 @@
 /**
+ * @description 贪心算法
  * @param {number[]} nums
  * @return {number}
  */
@@ -18,4 +19,19 @@ function maxSubArray(nums) {
 	return result;
 }
 
-maxSubArray([-1, -1, -1]);
+/**
+ * @description 动态规划
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray(nums) {
+	const n = nums.length;
+	const dp = new Array(n).fill(0);
+	dp[0] = nums[0];
+	let max = dp[0];
+	for (let i = 1; i < n; i++) {
+		dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+		max = Math.max(max, dp[i]);
+	}
+	return max;
+}
