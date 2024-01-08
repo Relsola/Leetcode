ee585ae2a56ca1dd3352a42199ec4d1bf39dff57
Relsola <sola001018@gmail.com>
2024-01-08 21:45:55 +0800

    JavaScript

--- JavaScript/0841-钥匙和房间.js
+++ JavaScript/0841-钥匙和房间.js
@@ -25,3 +25,33 @@ function canVisitAllRooms(rooms) {
   }
   return true;
 }
+
+/**
+ * @description 广度搜索优先
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+function canVisitAllRooms(rooms) {
+  function bfs(key, rooms, visited) {
+    const stack = [...rooms[key]];
+    visited[key] = true;
+    while (stack.length !== 0) {
+      const k = stack.shift();
+      if (!visited[k]) {
+        stack.push(...rooms[k]);
+        visited[k] = true;
+      }
+    }
+  }
+
+  const n = rooms.length;
+  const visited = new Array(n).fill(false);
+  bfs(0, rooms, visited);
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      return false;
+    }
+  }
+  return true;
+}
