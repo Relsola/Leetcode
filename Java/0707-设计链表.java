119d2fea9374249b1efdd50035edf4c0a296386e
Relsola <sola001018@gmail.com>
2024-01-10 21:09:03 +0800

    Java

--- Java/0707-设计链表.java
+++ Java/0707-设计链表.java
@@ -1,3 +1,4 @@
+// 单链表
 class ListNode {
   int val;
   ListNode next;
@@ -10,7 +11,6 @@ class ListNode {
   }
 }
 
-// 单链表
 class MyLinkedList {
   public int size;
   public ListNode head;
@@ -53,5 +53,75 @@ class MyLinkedList {
     ListNode pre = index == 0 ? head : getNode(index - 1);
     size--;
     pre.next = pre.next.next;
+  }
+}
+
+// 双链表
+class ListNode {
+  int val;
+  ListNode next, prev;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+}
+
+
+class MyLinkedList {
+  public int size;
+  public ListNode head, tail;
+
+  public MyLinkedList() {
+    size = 0;
+    head = new ListNode(0);
+    tail = new ListNode(0);
+    head.next = tail;
+    tail.prev = head;
+  }
+
+  private ListNode getNode(int index) {
+    if (index < 0 || index >= size) return null;
+    ListNode cur = head;
+    if (index >= size / 2) {
+      cur = tail;
+      while (size - index++ > 0) cur = cur.prev;
+    } else {
+      while (index-- >= 0) cur = cur.next;
+    }
+    return cur;
+  }
+
+  public int get(int index) {
+    return index < 0 || index >= size ? -1 : getNode(index).val;
+  }
+
+  public void addAtHead(int val) {
+    addAtIndex(0, val);
+  }
+
+  public void addAtTail(int val) {
+    addAtIndex(size, val);
+  }
+
+  public void addAtIndex(int index, int val) {
+    if (index > size) return;
+    ListNode pre = index <= 0 ? head : getNode(index - 1);
+    size++;
+    ListNode node = new ListNode(val);
+    node.next = pre.next;
+    node.prev = pre;
+    node.next.prev = node;
+    pre.next = node;
+  }
+
+  public void deleteAtIndex(int index) {
+    if (index < 0 || index >= size) return;
+    ListNode pre = index == 0 ? head : getNode(index - 1);
+    size--;
+    pre.next = pre.next.next;
+    pre.next.prev = pre;
   }
 }
