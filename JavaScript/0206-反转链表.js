ecbf5b3799804ccc5a522ce75082c5f1d00bfcba
Relsola <sola001018@gmail.com>
2024-01-11 21:40:38 +0800

    JavaScript

--- JavaScript/0206-反转链表.js
+++ JavaScript/0206-反转链表.js
@@ -1,53 +1,52 @@
-class ListNode {
-	val;
-	next = null;
-	constructor(value) {
-		this.val = value;
-		this.next = null;
-	}
+/** 双指针 */
+function reverseList(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  let temp = null,
+    pre = null,
+    cur = head;
+
+  while (cur !== null) {
+    temp = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = temp;
+  }
+  return pre;
 }
 
-// 双指针
+/** 递归（从前往后翻转） */
 function reverseList(head) {
-	if (head === null || head.next === null) return head;
-	let temp = null,
-		pre = null,
-		cur = head;
-
-	while (cur !== null) {
-		temp = cur.next;
-		cur.next = pre;
-		pre = cur;
-		cur = temp;
-	}
-	return pre;
+  function reverse(pre, cur) {
+    if (cur === null) {
+      return pre;
+    }
+    const temp = cur.next;
+    cur.next = pre;
+    return reverse(cur, temp);
+  }
+  return reverse(null, head);
 }
 
-// 递归（从前往后翻转）
+/** 递归（从后往前翻转） */
 function reverseList(head) {
-	const reverse = (pre, head) => {
-		if (head === null) return pre;
-		const temp = head.next;
-		head.next = pre;
-		pre = head;
-		return reverse(pre, temp);
-	};
-
-	return reverse(null, head);
-}
-
-// 递归（从后往前翻转）
-function reverseList(head) {
-	const reverse = head => {
-		if (head === null || head.next === null) return head;
-		const pre = reverse(head.next);
-		head.next = pre.next;
-		pre.next = head;
-		return head;
-	};
-
-	let cur = head;
-	while (cur !== null && cur.next != null) cur = cur.next;
-	reverse(head);
-	return cur;
+  if (head === null || head.next === null) {
+    return null;
+  }
+  function reverse(head) {
+    if (head.next === null) {
+      return head;
+    }
+    const pre = reverse(head.next);
+    head.next = null;
+    pre.next = head;
+    return head;
+  }
+  let cur = head;
+  while (cur !== null && cur.next !== null) {
+    cur = cur.next;
+  }
+  reverse(head);
+  return cur;
 }
