e582bacf4f1d652732fa70ed20b7633308a23b17
Relsola <sola001018@gmail.com>
2024-01-12 20:22:33 +0800

    JavaScript

--- JavaScript/0024-两两交换链表中的节点.js
+++ JavaScript/0024-两两交换链表中的节点.js
@@ -1,29 +1,27 @@
 class ListNode {
-	val;
-	next = null;
-	constructor(value) {
-		this.val = value;
-		this.next = null;
-	}
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
 function swapPairs(head) {
-	// 虚拟表头
-	const data = new ListNode(null, head);
-	let temp = data,
-		cur = null,
-		pre = null;
+  // 虚拟表头
+  const data = new ListNode(0, head);
+  let temp = data,
+    cur = null,
+    pre = null;
 
-	while (temp.next !== null && temp.next.next !== null) {
-		cur = temp.next.next;
-		pre = temp.next;
+  while (temp.next !== null && temp.next.next !== null) {
+    cur = temp.next.next;
+    pre = temp.next;
 
-		// 两两交换
-		pre.next = cur.next;
-		cur.next = pre;
-		temp.next = cur;
-		temp = pre;
-	}
+    // 两两交换
+    pre.next = cur.next;
+    cur.next = pre;
+    temp.next = cur;
+    temp = pre;
+  }
 
-	return data.next;
+  return data.next;
 }
