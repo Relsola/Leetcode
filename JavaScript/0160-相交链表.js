6560ed2ef688887ff777eb24cbee1a5b472bd6ba
Relsola <sola001018@gmail.com>
2024-01-14 21:28:24 +0800

    JavaScript

--- JavaScript/0160-相交链表.js
+++ JavaScript/0160-相交链表.js
@@ -32,3 +32,16 @@ function getIntersectionNode(headA, headB) {
   }
   return curA;
 }
+
+// 一个循环
+function getIntersectionNode(headA, headB) {
+  let p1 = headA,
+    p2 = headB;
+
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : headB;
+    p2 = p2 ? p2.next : headA;
+  }
+
+  return p1;
+}
