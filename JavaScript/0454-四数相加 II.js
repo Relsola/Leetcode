12da58826cca37bff43a494564b09796275748fc
Relsola <sola001018@gmail.com>
2024-01-24 18:45:32 +0800

    JavaScript

--- JavaScript/0454-四数相加 II.js
+++ JavaScript/0454-四数相加 II.js
@@ -1,19 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
 function fourSumCount(nums1, nums2, nums3, nums4) {
-	const twoSumMap = new Map();
-	let count = 0;
-	// 统计nums1和nums2数组元素之和，和出现的次数，放到map中
-	for (const n1 of nums1)
-		for (const n2 of nums2) {
-			const sum = n1 + n2;
-			twoSumMap.set(sum, (twoSumMap.get(sum) ?? 0) + 1);
-		}
-
-	// 找到如果 0-(c+d) 在map中出现过的话，就把map中key对应的value也就是出现次数统计出来
-	for (const n3 of nums3)
-		for (const n4 of nums4) {
-			const sum = n3 + n4;
-			count += twoSumMap.get(0 - sum) ?? 0;
-		}
-
-	return count;
+  const twoSumMap = new Map(),
+    n = nums1.length;
+  for (let i = 0; i < n; i++) {
+    for (let k = 0; k < n; k++) {
+      const sum = nums1[i] + nums2[k];
+      twoSumMap.set(sum, (twoSumMap.get(sum) || 0) + 1);
+    }
+  }
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    for (let k = 0; k < n; k++) {
+      const sum = nums3[i] + nums4[k];
+      result += twoSumMap.get(0 - sum) || 0;
+    }
+  }
+  return result;
 }
