1d0d2ac765f1049476fe655255feaf21bf7257b6
Relsola <sola001018@gmail.com>
2024-01-26 20:36:08 +0800

    JavaScript

--- JavaScript/0015-三数之和.js
+++ JavaScript/0015-三数之和.js
@@ -3,36 +3,43 @@
  * @return {number[][]}
  */
 function threeSum(nums) {
-	const result = [],
-		n = nums.length;
-	// 将数组排序
-	nums.sort((a, b) => a - b);
+  const result = [],
+    n = nums.length;
+  // 将数组排序
+  nums.sort((a, b) => a - b);
 
-	for (let i = 0; i < n - 2; i++) {
-		let l = i + 1,
-			r = n - 1,
-			iNum = nums[i];
-		// 数组排过序，如果第一个数大于0直接返回res
-		if (iNum > 0) return result;
-		// 去重
-		if (iNum === nums[i - 1]) continue;
+  let left = 0,
+    right = 0;
 
-		while (l < r) {
-			let lNum = nums[l],
-				rNum = nums[r],
-				threeSum = iNum + lNum + rNum;
+  for (let i = 0; i < n - 2; i++) {
+    left = i + 1;
+    right = n - 1;
+    iNum = nums[i];
+    // 数组排过序，如果第一个数大于0直接返回res
+    if (iNum > 0) {
+      return result;
+    }
+    // 去重
+    if (iNum === nums[i - 1]) {
+      continue;
+    }
 
-			// 三数之和小于0，则左指针向右移动
-			if (threeSum < 0) l++;
-			else if (threeSum > 0) r--;
-			else {
-				result.push([iNum, lNum, rNum]);
-				// 去重
-				while (l < r && nums[l] === nums[++l]);
-				while (l < r && nums[r] === nums[--r]);
-			}
-		}
-	}
+    while (left < right) {
+      let lNum = nums[left],
+        rNum = nums[right],
+        threeSum = iNum + lNum + rNum;
 
-	return result;
+      // 三数之和小于0，则左指针向右移动
+      if (threeSum < 0) left++;
+      else if (threeSum > 0) right--;
+      else {
+        result.push([iNum, lNum, rNum]);
+        // 去重
+        while (left < right && nums[left] === nums[++left]);
+        while (left < right && nums[right] === nums[--right]);
+      }
+    }
+  }
+
+  return result;
 }
