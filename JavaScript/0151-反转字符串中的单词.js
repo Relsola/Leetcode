30742d5f9978a54bcba3d9bf15a500d37dcd5ce6
Relsola <sola001018@gmail.com>
2024-01-28 20:19:32 +0800

    JavaScript

--- JavaScript/0151-反转字符串中的单词.js
+++ JavaScript/0151-反转字符串中的单词.js
@@ -3,24 +3,24 @@
  * @return {string}
  */
 function reverseWords(s) {
-	// 字符串转数组
-	const charArr = Array.from(s),
-		n = charArr.length;
-	// 移除多余空格
-	removeExtraSpace(charArr);
-	// 翻转
-	reverse(charArr, 0, n - 1);
+  // 字符串转数组
+  const charArr = Array.from(s),
+    n = charArr.length;
+  // 移除多余空格
+  removeExtraSpace(charArr);
+  // 翻转
+  reverse(charArr, 0, n - 1);
 
-	let start = 0;
-	for (let i = 0; i <= n; i++) {
-		if (charArr[i] === " " || i === n) {
-			// 翻转单词
-			reverse(charArr, start, i - 1);
-			start = i + 1;
-		}
-	}
+  let start = 0;
+  for (let i = 0; i <= n; i++) {
+    if (charArr[i] === ' ' || i === n) {
+      // 翻转单词
+      reverse(charArr, start, i - 1);
+      start = i + 1;
+    }
+  }
 
-	return charArr.join("");
+  return charArr.join('');
 }
 
 /**
@@ -29,24 +29,26 @@ function reverseWords(s) {
  * @return          {void}
  */
 function removeExtraSpace(charArr) {
-	let slowIndex = 0,
-		fastIndex = 0,
-		n = charArr.length;
+  let slowIndex = 0,
+    fastIndex = 0,
+    length = charArr.length;
 
-	// 移除开始位置的空格
-	while (fastIndex < n && charArr[fastIndex] === " ") fastIndex++;
+  // 移除开始位置的空格
+  while (fastIndex < length && charArr[fastIndex] === ' ') {
+    fastIndex++;
+  }
 
-	// 移除重复的空格
-	while (fastIndex < n) {
-		if (charArr[fastIndex] === " " && charArr[fastIndex - 1] === " ") {
-			fastIndex++;
-		} else {
-			charArr[slowIndex++] = charArr[fastIndex++];
-		}
-	}
+  // 移除重复的空格
+  while (fastIndex < length) {
+    if (charArr[fastIndex] === ' ' && charArr[fastIndex - 1] === ' ') {
+      fastIndex++;
+    } else {
+      charArr[slowIndex++] = charArr[fastIndex++];
+    }
+  }
 
-	// 移除末尾空格
-	charArr.length = charArr[slowIndex - 1] === " " ? slowIndex - 1 : slowIndex;
+  // 移除末尾空格
+  charArr.length = charArr[slowIndex - 1] === ' ' ? slowIndex - 1 : slowIndex;
 }
 
 /**
@@ -57,14 +59,14 @@ function removeExtraSpace(charArr) {
  * @return          {void}
  */
 function reverse(charArr, start, end) {
-	let temp;
+  let temp;
 
-	while (start < end) {
-		// 交换
-		temp = charArr[start];
-		charArr[start] = charArr[end];
-		charArr[end] = temp;
-		start++;
-		end--;
-	}
+  while (start < end) {
+    // 交换
+    temp = charArr[start];
+    charArr[start] = charArr[end];
+    charArr[end] = temp;
+    start++;
+    end--;
+  }
 }
