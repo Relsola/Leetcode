02fcfbaa805108de000aa651f9b6294b7f780bc1
Relsola <sola001018@gmail.com>
2024-02-02 19:28:19 +0800

    JavaScript

--- JavaScript/0028-找出字符串中第一个匹配项的下标.js
+++ JavaScript/0028-找出字符串中第一个匹配项的下标.js
@@ -5,39 +5,39 @@
  */
 
 function strStr(haystack, needle) {
-	/**
-	 * @description     获取前缀表
-	 * @param           {string} str
-	 * @return          {number[]}
-	 */
-	function getNext(str) {
-		const next = [];
-		let k = -1;
-		next[0] = k;
-		for (let i = 1, length = str.length; i < length; i++) {
-			while (k >= 0 && str[i] !== str[k + 1]) {
-				k = next[k];
-			}
-			if (str[i] === str[k + 1]) {
-				k++;
-			}
-			next[i] = k;
-		}
-		return next;
-	}
+  /**
+   * @description     获取前缀表
+   * @param           {string} str
+   * @return          {number[]}
+   */
+  function getNext(str) {
+    let k = -1;
+    const next = [-1];
 
-	const next = getNext(needle);
-	let k = -1;
-	for (let i = 0, length = haystack.length; i < length; i++) {
-		while (k >= 0 && haystack[i] !== needle[k + 1]) {
-			k = next[k];
-		}
-		if (haystack[i] === needle[k + 1]) {
-			if (k === needle.length - 2) {
-				return i - k - 1;
-			}
-			k++;
-		}
-	}
-	return -1;
+    for (let i = 1, length = str.length; i < length; i++) {
+      while (k >= 0 && str[i] !== str[k + 1]) {
+        k = next[k];
+      }
+      if (str[i] === str[k + 1]) {
+        k++;
+      }
+      next[i] = k;
+    }
+    return next;
+  }
+
+  const next = getNext(needle);
+  let k = -1;
+  for (let i = 0, length = haystack.length; i < length; i++) {
+    while (k >= 0 && haystack[i] !== needle[k + 1]) {
+      k = next[k];
+    }
+    if (haystack[i] === needle[k + 1]) {
+      if (k === needle.length - 2) {
+        return i - k - 1;
+      }
+      k++;
+    }
+  }
+  return -1;
 }
