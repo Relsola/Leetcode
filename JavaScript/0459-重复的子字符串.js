f0cfade5abf6591fd14862c495f46312cbbdcdbc
Relsola <sola001018@gmail.com>
2024-02-03 19:16:23 +0800

    update

--- JavaScript/0459-重复的子字符串.js
+++ JavaScript/0459-重复的子字符串.js
@@ -4,34 +4,34 @@
  */
 
 function repeatedSubstringPattern(s) {
-	/**
-	 * @description     获取前缀表
-	 * @param           {string} str
-	 * @return          {number[]}
-	 */
-	function getNext(str) {
-		const next = [];
-		let k = -1;
-		next[0] = k;
-		for (let i = 1, length = str.length; i < length; i++) {
-			while (k >= 0 && str[i] !== str[k + 1]) {
-				k = next[k];
-			}
-			if (str[i] === str[k + 1]) {
-				k++;
-			}
-			next.push(k);
-		}
-		return next;
-	}
+  /**
+   * @description     获取前缀表
+   * @param           {string} str
+   * @return          {number[]}
+   */
+  function getNext(str) {
+    const next = [];
+    let k = -1;
+    next[0] = k;
+    for (let i = 1, length = str.length; i < length; i++) {
+      while (k >= 0 && str[i] !== str[k + 1]) {
+        k = next[k];
+      }
+      if (str[i] === str[k + 1]) {
+        k++;
+      }
+      next.push(k);
+    }
+    return next;
+  }
 
-	const next = getNext(s);
-	const sLength = s.length;
-	const nextLength = next.length;
-	const suffixLength = next[nextLength - 1] + 1;
+  const next = getNext(s);
+  const sLength = s.length;
+  const nextLength = next.length;
+  const suffixLength = next[nextLength - 1] + 1;
 
-	if (suffixLength > 0 && sLength % (sLength - suffixLength) === 0) {
-		return true;
-	}
-	return false;
+  if (suffixLength > 0 && sLength % (sLength - suffixLength) === 0) {
+    return true;
+  }
+  return false;
 }
